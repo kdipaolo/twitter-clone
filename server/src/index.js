dc0e20f4fbd4832e6c91cb556c31688c53fb324b
Kurt Dipaolo <kurtdipaolo@gmail.com>
2018-10-18 12:51:59 -0400

    added apollo server boilerplate and cors package

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,40 @@
 import 'dotenv/config'
+import express from 'express'
+import { ApolloServer, gql } from 'apollo-server-express'
+import cors from 'cors'
 
-console.log('Hello World')
+const app = express()
+app.use(cors())
+
+const port = 8000
+
+const schema = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    username: String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: 'Kurt DiPaolo'
+      }
+    }
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers
+})
+
+server.applyMiddleware({ app, path: '/graphql' })
+
+app.listen({ port }, () => {
+  console.log(`Apollo Server on http://localhost:${port}/graphql`)
+})
