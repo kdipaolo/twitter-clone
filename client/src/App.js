8fbec7e0f59da49d1c1ff4f1d7097e257844f1f1
Kurt Dipaolo <kurtdipaolo@gmail.com>
2018-10-19 07:55:00 -0400

    cremoved unnecessary create react app files and set up react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './Components/Layout/Home'
+import Moments from './Components/Layout/Moments'
+import Notifications from './Components/Layout/Notifications'
+import Messages from './Components/Layout/Messages'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/moments" component={Moments} />
+            <Route exact path="/notifications" component={Notifications} />
+            <Route exact path="/messages" component={Messages} />
+          </Switch>
+        </Router>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ client/src/Components/Layout/Moments.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Moments extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Moments</h1>
+      </div>
+    )
+  }
+}
